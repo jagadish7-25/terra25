8ba438d39f755397e02194b735d5bf9cf544d715
jagadish7-25 <kadapajag7@gmail.com>
2025-03-18 16:42:21 +0100

    added

--- count/ec2.tf
+++ count/ec2.tf
@@ -24,8 +24,8 @@ resource "aws_security_group" "allow_all4" {
 }
 
 resource "aws_instance" "devops-terra" {
-  ami           = var.ami_id
   count = 3
+  ami           = var.ami_id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_all4.id]
   
