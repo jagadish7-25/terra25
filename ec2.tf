98cdd90dd35f6648a0a97c07fc16d2d7403c8272
jagadish7-25 <kadapajag7@gmail.com>
2025-03-18 12:51:21 +0100

    ec2 instance created

--- ec2.tf
+++ ec2.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "devops-terra" {
   ami           = "ami-09c813fb71547fc4f" 
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all4.id]
+  key_name = "terra25"
 
   tags = {
     Name = "devops-terra"
