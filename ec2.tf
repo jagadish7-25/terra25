8734cabb909f507804e7e6141e9601962cb2c17a
jagadish7-25 <kadapajag7@gmail.com>
2025-03-18 12:46:47 +0100

    instance added

--- ec2.tf
+++ ec2.tf
@@ -23,3 +23,12 @@ resource "aws_security_group" "allow_all4" {
   }
 }
 
+resource "aws_instance" "devops-terra" {
+  ami           = "ami-09c813fb71547fc4f" 
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all4.id]
+
+  tags = {
+    Name = "devops-terra"
+  }
+}
