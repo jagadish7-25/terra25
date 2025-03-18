dcacde71bac2703434393ee1bc77ca95ff2c9e5c
jagadish7-25 <kadapajag7@gmail.com>
2025-03-18 12:23:41 +0100

    provider and security group added

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_all4" {
+  name        = "allow_all4"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+
+  tags = {
+    Name = "allow_all4"
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
