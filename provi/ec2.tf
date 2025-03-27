f2c2b156f37ff3ac001fdb6d12b03a1c96f644c3
jagadish7-25 <kadapajag7@gmail.com>
2025-03-27 12:26:45 +0100

    files added

---
+++ provi/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "allow_all4" {
+  name        = "allow_all4"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  
+
+  tags = {
+    Name = var.sg_name
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "tcp"
+    cidr_blocks      = var.ing_cidr
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "devops-terra" {
+  ami           = var.ami_id
+  instance_type = var.inst_type
+  vpc_security_group_ids = [aws_security_group.allow_all4.id]
+  
+
+  tags = {
+    Name = "var.tags"
+  }
+  provisioner "local-execc" {
+    command = "echo ${self.public_ip} > public_ip.txt"
+  }
+  
+}
---
+++ provi/variables.tf
@@ -0,0 +1,39 @@
+variable "ami_id" {
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "this is the default ami id that we use for our terraform project "
+  
+}
+variable "inst_type" {
+    type = string
+    default = "t2.nano"
+    description = "this is the free source instance we used for terraform"
+  
+}
+
+
+variable "tags" {
+    type = map 
+    default = {
+        Name ="Backend"
+        Project = "Expense"
+        Component = "Backend"
+        Environmet = "Prod" 
+    }
+  
+}
+
+variable "sg_name" {
+    default= "allow_all4"
+
+}
+variable "from_port" {
+  default = "22"
+}
+variable "to_port" {
+  default = "22"
+}
+variable "ing_cidr" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
