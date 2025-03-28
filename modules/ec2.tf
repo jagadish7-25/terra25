d3eba00d699f125f457f0170ef05b4375110949e
jagadish7-25 <kadapajag7@gmail.com>
2025-03-28 11:25:17 +0100

    modified

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -25,7 +25,7 @@
 
 resource "aws_instance" "devops-terra" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = t2.micro
+  instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-09c956a01686bdc1d"]
   
 
