1a21cb6d585f979ad3d2ea9e0c2a026d5a39198f
jagadish7-25 <kadapajag7@gmail.com>
2025-03-28 12:46:19 +0100

    modified

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "ec2-demo" {
-    source = "git::https://github.com/jagadish7-25/terra25/modules/ec2-instance"
+    source = "git::https://github.com/jagadish7-25/terra25.git//modules/ec2-instance"
     instance_type = "t2.micro"
   
 }
