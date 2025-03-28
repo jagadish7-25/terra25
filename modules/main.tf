474cd7f47ea0e8d23b4712419d541e75771e8959
jagadish7-25 <kadapajag7@gmail.com>
2025-03-28 12:17:36 +0100

    modified

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,5 @@
 module "ec2-demo" {
-  source         = "./modules/ec2-instance"   # Change with a valid AMI ID
+  source         = "./terra25/modules/ec2-instance"   # Change with a valid AMI ID
   instance_type  = "t2.micro"
   instance_name  = "MyTerraformInstance"
 }
