54c416b1ef92554ae1750cff00344b2aca90e2cf
jagadish7-25 <kadapajag7@gmail.com>
2025-03-28 12:25:00 +0100

    folder removed

--- modules/ec2-instance/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "module-terra"
-  }
-}
--- modules/main.tf
+++
@@ -1,5 +0,0 @@
-module "ec2-demo" {
-  source         = "./modules/ec2-instance"   # Change with a valid AMI ID
-  instance_type  = "t2.micro"
-  instance_name  = "MyTerraformInstance"
-}
