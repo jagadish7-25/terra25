7f5020262deaff19d55983bfd521fa95818067e3
jagadish7-25 <kadapajag7@gmail.com>
2025-04-02 13:59:03 +0200

    modified

--- expense-projext/main.tf
+++ expense-projext/main.tf
@@ -107,6 +107,6 @@ resource "aws_nat_gateway" "expense-1" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_internet_gateway.expense-1]
 }
 
